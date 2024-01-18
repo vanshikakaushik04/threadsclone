07f4fbb4a5d629d29fdcc677eff017a7183c04cb
vanshikakaushik04 <156887479+vanshikakaushik04@users.noreply.github.com>
2024-01-18 20:42:25 +0530

    Add files via upload

---
+++ src/Routes/index.jsx
@@ -0,0 +1,44 @@
+import { createBrowserRouter } from "react-router-dom";
+import LoginPage from "../Pages/LoginPage";
+import SignUpPage from "../Pages/SignUpPage";
+import ThreadLayout from "../Layouts/ThreadLayout";
+import ProfileLayout from "../Layouts/ProfileLayout";
+import CreateThreadLayout from "../Layouts/CreateThreadLayout";
+import NotificationLayout from "../Layouts/NotificationLayout";
+import SingleThreadLayout from "../Layouts/SingleThreadLayout";
+import Replies from "../Components/Replies";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LoginPage />,
+  },
+  {
+    path: "/sign-up",
+    element: <SignUpPage />,
+  },
+  {
+    path: "/threads",
+    element: <ThreadLayout />,
+  },
+  {
+    path: "/profile",
+    element: <ProfileLayout />,
+  },
+  {
+    path: "/create-thread",
+    element: <CreateThreadLayout />,
+  },
+  {
+    path: "/notifications",
+    element: <NotificationLayout />,
+  },
+  {
+    path: "/thread-details",
+    element: <SingleThreadLayout />,
+  },
+  {
+    path: "/replies",
+    element: <Replies />,
+  },
+]);
